574f5ecfbf82938f4e92acbef1c059a90afdfe27
tom_glb <goodmorning_glb@yc.com>
2019-10-17 16:24:41 +0800

    init hcode-front and hcode-after

---
+++ hcode-after/src/main/java/com/yxh/hcode/HcodeAfterApplication.java
@@ -0,0 +1,13 @@
+package com.yxh.hcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcodeAfterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcodeAfterApplication.class, args);
+    }
+
+}
